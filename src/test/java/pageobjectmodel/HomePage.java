f91aa893c57b685158b6ce0a4e6acaba78fa1085
BalaTestPro <sakthibala.sengottiyan@testpro.io>
2024-05-18 01:42:57 -0400

    Updated HomePage.java
    Updated HomePage.java

--- src/test/java/pageobjectmodel/HomePage.java
+++ src/test/java/pageobjectmodel/HomePage.java
@@ -13,7 +13,8 @@ public class HomePage extends BasePage {
     }
 
     //WebElements
-    @FindBy(css = "#mainFooter div[class='side player-controls']")
+    //@FindBy(css = "#mainFooter div[class='side player-controls']")
+    @FindBy(xpath = "//i[@data-testid='play-next-btn']")
     private WebElement playerControlsBtn;
 
     @FindBy(css = "#mainFooter span[class='play']")
